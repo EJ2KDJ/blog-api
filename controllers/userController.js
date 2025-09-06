42646c2d7bbbd366b1b6a58dc5651200f947bdc5
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2025-09-06 17:18:25 +0800

    Retrieve specific User

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,20 @@ const getAllUsers = async (req, res, next) => {
         next(error);
     }
 };
+
+// Get user by ID from database
+const getUserById = async (req, res, next) => {
+    try {
+        //find User by specified ID and include Posts associated with that user
+        const user = await Users.findByPk(req.params.id, 
+            {
+            attributes: ['id', 'name', 'email', 'createdAt', 'updatedAt'],
+            include: [{ model: Posts }]
+            });
+        // If user doesn't exist, return error
+        if (!user) return req.status(404).json({ error: 'User not found' });
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+}
